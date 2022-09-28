5f774f71ab33f449db40c1bd8fff094131ed2c91
walter reis <walter-reis@outlook.com>
2022-09-28 15:33:33 -0300

    New versionupdate

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
